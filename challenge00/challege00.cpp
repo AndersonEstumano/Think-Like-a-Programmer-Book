f7f6b484712cd7031cc61ba006e307574a3c260d
Anderson Estumano <andersonestumano1@gmail.com>
2023-06-25 21:39:06 +0000

    Author:    Anderson Estumano <andersonestumano1@gmail.com>

---
+++ challenge00/challege00.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5 - i; j++)
+        {
+            std::cout << "#"; 
+        }
+        std::cout << "\n";
+        
+    }
+}
